d2a98265f255fd1ac4c659e339cccf16b857530e
abdul <basit.7ctech@gmail.com>
2024-04-06 11:43:35 -0700

    responsive

--- src/app/shared/footer/footer.jsx
+++ src/app/shared/footer/footer.jsx
@@ -12,60 +12,62 @@ const Footer = () => {
 
     return (
         <footer className={`${style.footer}`}>
-            <div className={`${style.footer_top}`}>
-                <div className={`${style.left}`}>
-                    <div className='w-75'>
-                        <span><img src="/pearl.png" /></span>
-                        <label>Subscribe <br /> to our newsletter</label>
+            <div className={`${style.container} container`}>
+                <div className={`${style.footer_top}`}>
+                    <div className={`${style.left}`}>
+                        <div className={``}>
+                            <span ><img src="/pearl.png" /></span>
+                            <label>Subscribe <br /> to our newsletter</label>
+                        </div>
+                    </div>
+                    <div className={`${style.right}`}>
+                        <div className={`d-flex justify-content-between`}>
+                            <span>
+                                <label>Company</label>
+                                <ul>
+                                    <li>Home</li>
+                                    <li>About Us</li>
+                                    <li>Projects</li>
+                                    <li>Contact Us</li>
+                                </ul>
+                            </span>
+                            <span>
+                                <label>Contact Us</label>
+                                <ul>
+                                    <li>
+                                        <FontAwesomeIcon icon={faEnvelope} />
+                                        <label className='ms-2'>Info@pearlepp.co.uk</label>
+                                    </li>
+                                    <li>
+                                        <FontAwesomeIcon icon={faPhone} />
+                                        <label className='ms-2'>+44 7391 569139</label>
+                                    </li>
+                                    <li className={`${style.wrap}`}>
+                                        <FontAwesomeIcon icon={faLocationDot} />
+                                        <label className='ms-2'>Flat 02 Towerfieds, Westerham Road Bromley, BR2 6HF,London,United Kingdom</label>
+                                    </li>
+                                </ul>
+                            </span>
+                        </div>
                     </div>
                 </div>
-                <div className={`${style.right}`}>
-                    <div className={`w-75 ms-5 d-flex justify-content-between`}>
+                <div className={`${style.footer_bottom}`}>
+                    <div className={`${style.copy_right}`}>© Copyright {year.getFullYear()} PERAL.</div>
+                    <div className={`${style.social_terms}`}>
                         <span>
-                            <label>Company</label>
                             <ul>
-                                <li>Home</li>
-                                <li>About Us</li>
-                                <li>Projects</li>
-                                <li>Contact Us</li>
+                                <li><FontAwesomeIcon icon={faFacebookF} /></li>
+                                <li><FontAwesomeIcon icon={faLinkedin} /></li>
+                                <li><FontAwesomeIcon icon={faX} /></li>
+                                <li><FontAwesomeIcon icon={faInstagram} /></li>
+                                <li><FontAwesomeIcon icon={faYoutube} /></li>
                             </ul>
                         </span>
                         <span>
-                            <label>Contact Us</label>
-                            <ul>
-                                <li>
-                                    <FontAwesomeIcon icon={faEnvelope} />
-                                    <label className='ms-2'>Info@pearlepp.co.uk</label>
-                                </li>
-                                <li>
-                                    <FontAwesomeIcon icon={faPhone} />
-                                    <label className='ms-2'>+44 7391 569139</label>
-                                </li>
-                                <li>
-                                    <FontAwesomeIcon icon={faLocationDot} />
-                                    <label className='ms-2'>Flat 02 Towerfieds, Westerham Road Bromley, BR2 6HF,London,United Kingdom</label>
-                                </li>
-                            </ul>
+                            <a href="#">Privacy Policy</a>
+                            <a href="#">Terms of Services</a>
                         </span>
                     </div>
-                </div>
-            </div>
-            <div className={`${style.footer_bottom}`}>
-                <div className={`${style.copy_right}`}>© Copyright {year.getFullYear()} PERAL.</div>
-                <div className={`${style.social_terms}`}>
-                    <span>
-                        <ul>
-                            <li><FontAwesomeIcon icon={faFacebookF} /></li>
-                            <li><FontAwesomeIcon icon={faLinkedin} /></li>
-                            <li><FontAwesomeIcon icon={faX} /></li>
-                            <li><FontAwesomeIcon icon={faInstagram} /></li>
-                            <li><FontAwesomeIcon icon={faYoutube} /></li>
-                        </ul>
-                    </span>
-                    <span>
-                        <a href="#">Privacy Policy</a>
-                        <a href="#">Terms of Services</a>
-                    </span>
                 </div>
             </div>
         </footer>
