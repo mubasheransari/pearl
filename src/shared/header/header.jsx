f51e0053fb095b93f4964ae0b04ea939af2bb37c
mmalahim <m.malahim57@gmail.com>
2024-05-15 07:16:17 +0500

    form work

--- src/shared/header/header.jsx
+++ src/shared/header/header.jsx
@@ -24,7 +24,7 @@ const Header = () => {
     { link: '/#expertise', class: 'nav-link', text: 'Our Expertise' },
   ]
 
-  const dropdowLinks = [
+  const categoryLinks = [
     { link: '/quantity_surverying', class: 'dropdown-item', text: 'Quantity Surverying' },
     { link: '/contracts_management', class: 'dropdown-item', text: 'Contracts Management' },
     { link: '/snagging_services', class: 'dropdown-item', text: 'Snagging Works' },
@@ -33,6 +33,9 @@ const Header = () => {
     { link: '/quality_management', class: 'dropdown-item', text: 'Quality Management' },
     { link: '/commercial_management', class: 'dropdown-item', text: 'Commercial Management' },
     { link: '/project_management', class: 'dropdown-item', text: 'Project Management' },
+  ]
+
+  const servicesLinks = [
     { link: '/architectural_services', class: 'dropdown-item', text: 'Architectural Services' },
     { link: '/structural_enigneering_services', class: 'dropdown-item', text: 'Structural Enigneering Services' },
     { link: '/planning_and_building_control_services', class: 'dropdown-item', text: 'Planning and Building Control Services' },
@@ -58,55 +61,71 @@ const Header = () => {
 
   return (
     <header className={style.header}>
-    <nav ref={headerRef} className={`navbar navbar-expand-md navbar-dark fixed-top bg-dark ${style.scroll}`}>
-      <div className={`container-fluid ${style.header_container}`}>
-        <div className={`${style.logo_box}`}>
-          <span><img src="/pearl.png" /></span>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-            aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <FontAwesomeIcon icon={faBarsStaggered} />
-          </button>
-        </div>
-        <div className={`collapse navbar-collapse ${style.header_links}`} id="navbarCollapse">
-          <ul className="navbar-nav mb-2 mb-md-0">
-            {navLinks.map((link, i) => {
-              // ${(location?.hash == link.link?.replace('/', '')) ? style.active : ''}
-              return (
-                <>
-                  <li className={`nav-item`} key={'nav_' + i}>
-                    <Link className={`${link.class}`} href={link.link}>{link.text}</Link>
-                  </li>
-                </>
-              )
-            })}
-            <div className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Categories
-              </a>
-              <ul className={` dropdown-menu ${style.dropdown_menu}`} aria-labelledby="navbarDropdownMenuLink">
-                {dropdowLinks.map((link, i) => {
-                  return (
-                    <>
-                      <li>
-                        <Link className={`${link.class}`} href={link.link} >{link.text}</Link>
-                      </li>
-                    </>
-                  )
-                })}
-              </ul>
-            </div>
-            <li className={`nav-item ${style.contact}`}>
-              <Link className={`nav-link`} href="/#contact-us">Contact Us</Link>
-            </li>
-            <li className={`nav-item ${style.form}`}>
-              <Link className={`nav-link`} href="/form">Form</Link>
-            </li>
+      <nav ref={headerRef} className={`navbar navbar-expand-md navbar-dark fixed-top bg-dark ${style.scroll}`}>
+        <div className={`container-fluid ${style.header_container}`}>
+          <div className={`${style.logo_box}`}>
+            <span><img src="/pearl.png" /></span>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+              aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+              <FontAwesomeIcon icon={faBarsStaggered} />
+            </button>
+          </div>
+          <div className={`collapse navbar-collapse ${style.header_links}`} id="navbarCollapse">
+            <ul className="navbar-nav mb-2 mb-md-0">
+              {navLinks.map((link, i) => {
+                // ${(location?.hash == link.link?.replace('/', '')) ? style.active : ''}
+                return (
+                  <>
+                    <li className={`nav-item`} key={'nav_' + i}>
+                      <Link className={`${link.class}`} href={link.link}>{link.text}</Link>
+                    </li>
+                  </>
+                )
+              })}
+              <div className={` nav-item dropdown ${style.dropdown_div}`}>
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Services
+                </a>
+                <ul className={` dropdown-menu ${style.dropdown_menu}`} aria-labelledby="navbarDropdownMenuLink">
+                  {servicesLinks.map((link, i) => {
+                    return (
+                      <>
+                        <li>
+                          <Link className={`${link.class}`} href={link.link} >{link.text}</Link>
+                        </li>
+                      </>
+                    )
+                  })}
+                </ul>
+              </div>
+              <div className={` nav-item dropdown ${style.dropdown_div}`}>
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Categories
+                </a>
+                <ul className={` dropdown-menu ${style.dropdown_menu}`} aria-labelledby="navbarDropdownMenuLink">
+                  {categoryLinks.map((link, i) => {
+                    return (
+                      <>
+                        <li>
+                          <Link className={`${link.class}`} href={link.link} >{link.text}</Link>
+                        </li>
+                      </>
+                    )
+                  })}
+                </ul>
+              </div>
+              <li className={`nav-item ${style.contact}`}>
+                <Link className={`nav-link`} href="/#contact-us">Contact Us</Link>
+              </li>
+              <li className={`nav-item ${style.form}`}>
+                <Link className={`nav-link`} href="/form">Form</Link>
+              </li>
 
-          </ul>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </header>
+      </nav>
+    </header>
   )
 }
 
