f0f67bcdb46c2fd5ed8cb82b33fb46fc8450cd2c
ali1452 <aliabbas.mkting@gmail.com>
2024-11-05 23:30:00 +0500

    changes  about us

--- src/shared/about/about.jsx
+++ src/shared/about/about.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import style from './style.module.scss'
 import { Lora } from "next/font/google";
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faInstagram, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
 const lora = Lora({ subsets: ["latin"] });
 
 function About() {
@@ -8,12 +12,20 @@ function About() {
         <section className={`${style.about}`} id="about-us">
             <div className={`${style.about_box} container py-4 d-flex`}>
                 <div className={`${style.left}`}>
-                    <h5 className={lora.className}>About Us</h5>
+                    <div className={`${style.about_text}`}>
+                    <h5 className={lora.className} >Vision</h5>
+                        <p>
+                        To benefit society and built environments through resilient structural engineering that enhances safety and sustainability using innovative solutions.
+                        </p>
+                    </div>
+
                     <div className={`${style.image_box}`}>
                         <img src="/about.jpg" alt="about image" />
                     </div>
                     <div className={`${style.about_text}`}>
-                        <p>
+                    <h5 className={lora.className}>About Us</h5>
+                        
+                                            <p>
                             Pearl Engineers Planners and Project Managers is an emerging engineering Consultancy firm dedicated to the residential and commercial construction sector in the United Kingdom. With a passion for excellence and a commitment to deliver exceptionalresults, we bring expertise, innovation, and reliability to every project we undertake.
                         </p>
                         <p>
@@ -26,7 +38,25 @@ function About() {
                     <label>Resilience</label>
                     <label className={`${style.strok}`}>Intelligence</label>
                 </div>
+                
             </div>
+            <div style={{padding:'10px'}}>
+            <div className={style.social_Link} >
+            <div>  
+            <h4 className={lora.className}>Get Connected</h4>
+            <span>
+            <ul>
+                <Link href='#'><li><FontAwesomeIcon icon={faFacebookF} /></li></Link>
+                <Link href='#'><li><FontAwesomeIcon icon={faLinkedin} /></li></Link>
+                <Link href='#'><li><FontAwesomeIcon icon={faInstagram} /></li></Link>
+                <Link href='#'><li><FontAwesomeIcon icon={faYoutube} /></li></Link>
+            </ul>
+            </span>
+            </div>
+            </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2491.0280513620164!2d0.038921!3d51.365781!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTHCsDIxJzU2LjgiTiAwwrAwMicyMC4xIkU!5e0!3m2!1sen!2s!4v1713013995125!5m2!1sen!2s" width="100%" height="500" style={{ border: '0' }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+          
         </section>
     )
 }
