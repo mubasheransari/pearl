91ada671c7330d3c4d7f54e3c065accbeab6ec62
ali1452 <aliabbas.mkting@gmail.com>
2025-02-05 12:54:30 +0500

    fix video play

--- src/container/home/video.jsx
+++ src/container/home/video.jsx
@@ -9,18 +9,35 @@ const lora = Lora({ subsets: ["latin"] });
 const Video = () => {
     const videoRef = useRef();
 
+    // useEffect(() => {
+    //   const video = videoRef.current;
+    //   if (video) {
+    //     video.muted = true;
+    //     video.play().catch(error => {
+    //       console.error('Autoplay was prevented:', error);
+    //     });
+    //     setTimeout(() => {
+    //       video.muted = false;
+    //     }, 3000); // Unmute after 3 seconds
+    //   }
+    // }, []);
+
     useEffect(() => {
       const video = videoRef.current;
       if (video) {
         video.muted = true;
-        video.play().catch(error => {
-          console.error('Autoplay was prevented:', error);
-        });
-        setTimeout(() => {
-          video.muted = false;
-        }, 3000); // Unmute after 3 seconds
+        video.play()
+          .then(() => {
+            setTimeout(() => {
+              video.muted = false;
+              video.currentTime = 0; // Restart to ensure playback
+              video.play(); // Restart playback after unmuting
+            }, 3000);
+          })
+          .catch((error) => console.error("Autoplay prevented:", error));
       }
     }, []);
+    
   return (
     <div className={style.home} id="home">
         <div className={`${style.back_layer}`}></div>
