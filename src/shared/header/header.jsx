5758478a42cb2b26ca6473e952f0a17bf32468f1
Hammadprogrammer <hammadzahid221@gamil.com>
2025-02-05 11:04:29 -0800

    Your commit message

--- src/shared/header/header.jsx
+++ src/shared/header/header.jsx
@@ -1,116 +1,140 @@
-'use client'
-import React, { useEffect, useRef, useState } from 'react'
-import style from './style.module.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBarsStaggered, faBuilding, faTools, faRulerCombined, faClipboardCheck, faHardHat, faBriefcase, faProjectDiagram, faBalanceScale, faArchway, faDraftingCompass, faCalendarCheck } from '@fortawesome/free-solid-svg-icons'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+'use client';
+import React, { useState, useEffect, useRef } from 'react';
+import Link from 'next/link';
+import styles from './style.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faArchway,
+  faDraftingCompass,
+  faCalendarCheck,
+  faRulerCombined,
+  faClipboardCheck,
+  faTools,
+  faBalanceScale,
+  faHardHat,
+  faBuilding,
+  faBriefcase,
+  faProjectDiagram,
+  faPen
+} from '@fortawesome/free-solid-svg-icons';
 
-const Header = () => {
-  const headerRef = useRef()
-  const pathname = usePathname()
-  const [path, setPath] = useState(null)
+const servicesLinks = [
+  { link: '/architectural_services', text: 'Architectural Services', icon: faArchway },
+  { link: '/structural_engineering_services', text: 'Structural Engineering Services', icon: faDraftingCompass },
+  { link: '/planning_and_building_control_services', text: 'Planning & Building Control', icon: faCalendarCheck },
+];
+
+const categoryLinks = [
+  { link: '/quantity_surveying', text: 'Quantity Surveying', icon: faRulerCombined },
+  { link: '/contracts_management', text: 'Contracts Management', icon: faClipboardCheck },
+  { link: '/snagging_services', text: 'Snagging Works', icon: faTools },
+  { link: '/cost_control_value', text: 'Cost Control & Value Engineering', icon: faBalanceScale },
+  { link: '/contractor_management', text: 'Contractor Management', icon: faHardHat },
+  { link: '/quality_management', text: 'Quality Management', icon: faBuilding },
+  { link: '/commercial_management', text: 'Commercial Management', icon: faBriefcase },
+  { link: '/project_management', text: 'Project Management', icon: faProjectDiagram },
+];
+
+const blogLinks = [
+  { link: '/blog/post1', text: 'Blog Post 1', icon: faPen },
+  { link: '/blog/post2', text: 'Blog Post 2', icon: faPen },
+  { link: '/blog/post3', text: 'Blog Post 3', icon: faPen },
+];
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [servicesOpen, setServicesOpen] = useState(false);
+  const [categoriesOpen, setCategoriesOpen] = useState(false);
+  const [blogOpen, setBlogOpen] = useState(false);
+
+  const sidebarRef = useRef(null);
+  const dropdownRef = useRef(null);
 
   useEffect(() => {
-    setPath(pathname)
-  }, [pathname])
-
-  const navLinks = [
-    { link: '/', text: 'Home' },
-    { link: '/about', text: 'About Us' },
-    { link: '/#services', text: 'Our Vision' },
-    { link: '/#expertise', text: 'Our Expertise' },
-  ]
-
-  const categoryLinks = [
-    { link: '/quantity_surverying', text: 'Quantity Surverying', icon: faRulerCombined },
-    { link: '/contracts_management', text: 'Contracts Management', icon: faClipboardCheck },
-    { link: '/snagging_services', text: 'Snagging Works', icon: faTools },
-    { link: '/cost_control_value', text: 'Cost Control & Value Engineering', icon: faBalanceScale },
-    { link: '/contractor_management', text: 'Contractor Management', icon: faHardHat },
-    { link: '/quality_management', text: 'Quality Management', icon: faBuilding },
-    { link: '/commercial_management', text: 'Commercial Management', icon: faBriefcase },
-    { link: '/project_management', text: 'Project Management', icon: faProjectDiagram },
-  ]
-
-  const servicesLinks = [
-    { link: '/architectural_services', text: 'Architectural Services', icon: faArchway },
-    { link: '/structural_enigneering_services', text: 'Structural Engineering Services', icon: faDraftingCompass },
-    { link: '/planning_and_building_control_services', text: 'Planning & Building Control Services', icon: faCalendarCheck },
-  ]
+    const handleClickOutside = (event) => {
+      if (
+        sidebarRef.current && !sidebarRef.current.contains(event.target) &&
+        dropdownRef.current && !dropdownRef.current.contains(event.target)
+      ) {
+        setMenuOpen(false);
+        setServicesOpen(false);
+        setCategoriesOpen(false);
+        setBlogOpen(false);
+      }
+    };
+    
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   return (
-    <header className={style.header}>
-      <nav ref={headerRef} className={`navbar navbar-expand-md navbar-dark fixed-top bg-dark ${style.scroll}`}>
-        <div className={`container-fluid ${style.header_container}`}>
-          <div className={style.logo_box}>
-            <span><Link href="/"><img src="/pearl.png" alt="Logo" /></Link></span>
-            <div className={style.logo_wrap}>
-              <p className={style.quotation_btn}>
-                <Link href="/form" className={style.instant_quote_btn}>Instant quote</Link>
-              </p>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <FontAwesomeIcon icon={faBarsStaggered} />
-              </button>
-            </div>
-          </div>
+    <div className={styles.maindiv}>
+      <nav className={styles.navbar} ref={dropdownRef}>
+        <div className={styles.navbar__logo}>
+          <Link href="/">
+            <img src="/pearl.png" alt="Logo" />
+          </Link>
+        </div>
 
-          <div className={`collapse navbar-collapse ${style.header_links}`} id="navbarCollapse">
-            <ul className="navbar-nav mb-2 mb-md-0">
-              {navLinks.map((link, i) => (
-                <li key={i} className="nav-item">
-                  <Link className="nav-link" href={link.link}>{link.text}</Link>
+        <ul className={styles.navbar__menu}>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About Us</Link></li>
+          <li><Link href="/#services">Our Vision</Link></li>
+          <li><Link href="/#expertise">Our Expertise</Link></li>
+
+          <li className={`${styles.dropdown} ${styles.desktopDropdown}`} onClick={() => setServicesOpen(!servicesOpen)}>
+            <Link href="#">Services ▾</Link>
+            <ul className={`${styles.dropdownMenu} ${servicesOpen ? styles.open : ''}`}>
+              {servicesLinks.map((service) => (
+                <li key={service.link}>
+                  <Link href={service.link}>
+                    <FontAwesomeIcon icon={service.icon} className={styles.icon} /> {service.text}
+                  </Link>
                 </li>
               ))}
-
-              {/* Services Dropdown */}
-              <div className={`nav-item dropdown ${style.dropdown_div}`}>
-                <a className="nav-link dropdown-toggle" href="#" id="servicesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Services
-                </a>
-                <ul className={`dropdown-menu ${style.dropdown_menu_service}`} aria-labelledby="servicesDropdown">
-                  {servicesLinks.map((link, i) => (
-                    <li key={i} className={style.category_item}>
-                      <FontAwesomeIcon icon={link.icon} className={style.category_icon} />
-                      <Link className="dropdown-item" href={link.link}>{link.text}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-
-              {/* Categories Dropdown */}
-              <div className={`nav-item dropdown ${style.dropdown_div}`}>
-                <a className="nav-link dropdown-toggle" href="#" id="categoriesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Categories
-                </a>
-                <ul className={`dropdown-menu ${style.dropdown_menu}`} aria-labelledby="categoriesDropdown">
-                  <div className={style.category_grid}>
-                    {categoryLinks.map((link, i) => (
-                      <li key={i} className={style.category_item}>
-                        <FontAwesomeIcon icon={link.icon} className={style.category_icon} />
-                        <Link className="dropdown-item" href={link.link}>{link.text}</Link>
-                      </li>
-                    ))}
-                  </div>
-                </ul>
-              </div>
-
-              <li className={`nav-item ${style.contact}`}>
-                <Link className="nav-link" href="/career">Career</Link>
-              </li>
-              <li className={`nav-item ${style.contact}`}>
-                <Link className="nav-link" href="/contact">Contact Us</Link>
-              </li>
-              <li className={`nav-item ${style.form}`}>
-                <Link className={`nav-link ${style.instant_quote_btn}`} href="/form">Instant quote</Link>
-              </li>
             </ul>
-          </div>
+          </li>
+
+          <li className={`${styles.dropdown} ${styles.desktopDropdown}`} onClick={() => setCategoriesOpen(!categoriesOpen)}>
+            <Link href="#">Categories ▾</Link>
+            <ul className={`${styles.dropdownMenu} ${categoriesOpen ? styles.open : ''}`}>
+              {categoryLinks.map((category) => (
+                <li key={category.link}>
+                  <Link href={category.link}>
+                    <FontAwesomeIcon icon={category.icon} className={styles.icon} /> {category.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li>
+
+          {/* <li className={`${styles.dropdown} ${styles.desktopDropdown}`} onClick={() => setBlogOpen(!blogOpen)}>
+            <Link href="#">Blog ▾</Link>
+            <ul className={`${styles.dropdownMenu} ${blogOpen ? styles.open : ''}`}>
+              {blogLinks.map((blog) => (
+                <li key={blog.link}>
+                  <Link href={blog.link}>
+                    <FontAwesomeIcon icon={blog.icon} className={styles.icon} /> {blog.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </li> */}
+
+          <li><Link href="/career">Career</Link></li>
+          <li><Link href="/sustainability">Sustainability</Link></li>
+          <li><Link href="/contact">Contact Us</Link></li>
+          <li><Link href="/quote">Instant Quote</Link></li>
+        </ul>
+
+        <div className={styles.navbar__hamburger} onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
         </div>
       </nav>
-    </header>
-  )
-}
+    </div>
+  );
+};
 
-export default Header
+export default Navbar;
