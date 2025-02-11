346f18b29cc366f4cf0605f58acd4fbfc1a5eec1
Hammadprogrammer <hammadzahid221@gmail.com>
2025-02-11 23:35:39 +0500

    contact form

---
+++ src/shared/potfolio/potfolio.jsx
@@ -0,0 +1,17 @@
+// import React from "react";
+// import Image from "next/image";
+// import img1 from "../../../potfolio-Image";
+// import img2 from "@/public/portfolio/image2.jpeg";
+// import img3 from "@/public/portfolio/image3.jpeg";
+
+// const images = [img1, img2, img3];
+
+// export default function PortfolioImages() {
+//   return (
+//     <div className="grid grid-cols-3 gap-4">
+//       {images.map((src, index) => (
+//         <Image key={index} src={src} alt={`Portfolio ${index + 1}`} width={200} height={200} />
+//       ))}
+//     </div>
+//   );
+// }
